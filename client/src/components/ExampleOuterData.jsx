fe1515b247c928fe42417cc29de5bee53b767574
dean-kim <keunu07@gmail.com>
2016-11-14 16:49:14 +0900

    dummy data

---
+++ client/src/components/ExampleOuterData.jsx
@@ -0,0 +1,3 @@
+window.exampleOuterData = [
+{"src":"http://static.zara.net/photos///2016/I/0/2/p/6000/537/505/2/w/560/6000537505_2_1_1.jpg?ts=1478596779700"},
+{"src":"http://static.zara.net/photos///2016/I/0/2/p/0706/211/800/2/w/560/0706211800_2_1_1.jpg?ts=1478711839142"}]
